93c46e9277ce0256bfbbec1fcb904d9a75f93c34
jorgeucano <jorgeucano@gmail.com>
2022-08-28 19:25:05 -0300

    initial commit

---
+++ apps/storiette-wc/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'storiette-wc',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/storiette-wc',
+};
